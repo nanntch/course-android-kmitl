e53c455a65d067143cc2e203341b9dc7ab2167c7
Student <student@LAB308-14.it.kmitl.ac.th>
2017-10-20 11:53:02 +0700

    for HW8

---
+++ lab/lab08/DemoRecord/app/src/androidTest/java/com/project/demorecord/MainActivityTest2.java
@@ -0,0 +1,85 @@
+package com.project.demorecord;
+
+
+import android.os.SystemClock;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest2 {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class); //.class,true,false);
+
+    @Test
+    public void mainActivityTest2() {
+//        ViewInteraction button = onView(
+//                allOf(withId(R.id.buttonAdded),
+//                        childAtPosition(
+//                                childAtPosition(
+//                                        withId(android.R.id.content),
+//                                        0),
+//                                6),
+//                        isDisplayed()));
+//        button.check(matches(isDisplayed()));
+
+        onView(withId(R.id.editTExtName)).perform(replaceText("Nacha1"), closeSoftKeyboard());
+        SystemClock.sleep(3000);
+        onView(withId(R.id.editTextAge)).perform(replaceText("20"), closeSoftKeyboard());
+        SystemClock.sleep(3000);
+        onView(allOf(withId(R.id.buttonAdded), withText("ADDED"))).perform(click());
+        onView(allOf(withId(R.id.buttonGotoList), withText("GO TO LIST"))).perform(click());
+        SystemClock.sleep(3000);
+
+        onView(withText("Nacha1")).check(matches(isDisplayed()));
+//        onView(withText(R.id.editTextName)).check(matches(withText("Nacha")));
+    }
+
+    @Test
+    public void bbb(){
+
+    }
+
+//    private static Matcher<View> childAtPosition(
+//            final Matcher<View> parentMatcher, final int position) {
+//
+//        return new TypeSafeMatcher<View>() {
+//            @Override
+//            public void describeTo(Description description) {
+//                description.appendText("Child at position " + position + " in parent ");
+//                parentMatcher.describeTo(description);
+//            }
+//
+//            @Override
+//            public boolean matchesSafely(View view) {
+//                ViewParent parent = view.getParent();
+//                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+//                        && view.equals(((ViewGroup) parent).getChildAt(position));
+//            }
+//        };
+//    }
+}
